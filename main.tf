5712ffbb29ae07138fa6f9bd8736eb7866fc950e
sorayeon <jihwan77@gmail.com>
2023-02-05 22:04:31 +0900

    Delete Nat GW

--- main.tf
+++ main.tf
@@ -29,9 +29,9 @@ module "default_vpc" {
   private_subnets = ["10.0.0.0/24", "10.0.1.0/24"]
   public_subnets  = ["10.0.100.0/24", "10.0.101.0/24"]
 
-  enable_nat_gateway     = true
+  /* enable_nat_gateway     = true
   single_nat_gateway     = false
-  one_nat_gateway_per_az = true
+  one_nat_gateway_per_az = true */
 
   tags = {
     Terraform   = "true"
