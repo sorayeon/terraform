f3d51d6dae245633ad545e84c9632e564d42b3da
sorayeon <jihwan77@gmail.com>
2023-02-05 22:01:16 +0900

    Refactoring terraform module

--- custom_vpc/local.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  az_a = "ap-northeast-2a"
-  az_c = "ap-northeast-2c"
-}
--- main.tf
+++ main.tf
@@ -19,7 +19,22 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-module "main_vpc" {
-  source = "./custom_vpc"
-  env    = terraform.workspace
+module "default_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "default_vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-2a", "ap-northeast-2c"]
+  private_subnets = ["10.0.0.0/24", "10.0.1.0/24"]
+  public_subnets  = ["10.0.100.0/24", "10.0.101.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = terraform.workspace
+  }
 }
