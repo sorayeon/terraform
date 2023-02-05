478b8054b68c54ed406335a30549e2a85f87eaa4
sorayeon <jihwan77@gmail.com>
2023-02-05 22:39:18 +0900

    Change name tag and security group name

--- main.tf
+++ main.tf
@@ -22,13 +22,14 @@ provider "aws" {
 module "default_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "default_vpc"
+  name = "default-vpc-${terraform.workspace}"
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-northeast-2a", "ap-northeast-2c"]
   private_subnets = ["10.0.0.0/24", "10.0.1.0/24"]
   public_subnets  = ["10.0.100.0/24", "10.0.101.0/24"]
 
+  manage_default_security_group = true
   /* enable_nat_gateway     = true
   single_nat_gateway     = false
   one_nat_gateway_per_az = true */
