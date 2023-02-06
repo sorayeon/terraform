fbb57ee4ae4e3970d754073ece99dde4af510495
sorayeon <jihwan77@gmail.com>
2023-02-06 22:28:39 +0900

    Add elb

--- main.tf
+++ main.tf
@@ -50,3 +50,38 @@ resource "aws_instance" "web_instances" {
     Name = "web-instance-${count.index}"
   }
 }
+
+module "web_elb_http" {
+  source  = "terraform-aws-modules/elb/aws"
+  version = "~> 2.0"
+
+  name            = "web-elb-http"
+  subnets         = module.default_vpc.public_subnets
+  security_groups = [module.default_vpc.default_security_group_id]
+  internal        = false
+
+  listener = [
+    {
+      instance_port     = 80
+      instance_protocol = "HTTP"
+      lb_port           = 80
+      lb_protocol       = "HTTP"
+    }
+  ]
+
+  health_check = {
+    target              = "HTTP:80/"
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+  }
+
+  access_logs = {
+    bucket = "mydata-s3"
+  }
+
+  number_of_instances = 2
+  instances           = aws_instance.web_instances[*].id
+
+}
