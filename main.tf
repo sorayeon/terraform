ea07aeddfe902870135d53e35843f4fc611ff5de
sorayeon <jihwan77@gmail.com>
2023-02-06 22:16:16 +0900

    Add ec2 instance and update vpc module

--- main.tf
+++ main.tf
@@ -25,9 +25,9 @@ module "default_vpc" {
   name = "default-vpc-${terraform.workspace}"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-northeast-2a", "ap-northeast-2c"]
-  private_subnets = ["10.0.0.0/24", "10.0.1.0/24"]
-  public_subnets  = ["10.0.100.0/24", "10.0.101.0/24"]
+  azs = ["ap-northeast-2a", "ap-northeast-2c"]
+  /*private_subnets = ["10.0.0.0/24", "10.0.1.0/24"]*/
+  public_subnets = ["10.0.100.0/24", "10.0.101.0/24"]
 
   manage_default_security_group = true
   /* enable_nat_gateway     = true
@@ -37,5 +37,16 @@ module "default_vpc" {
   tags = {
     Terraform   = "true"
     Environment = terraform.workspace
+  }
+}
+
+resource "aws_instance" "web_instances" {
+  count         = 2
+  ami           = "ami-013218fccb68a90d4"
+  instance_type = "t2.micro"
+  subnet_id     = module.default_vpc.public_subnets[count.index]
+
+  tags = {
+    Name = "web-instance-${count.index}"
   }
 }
